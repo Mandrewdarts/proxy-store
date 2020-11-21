0f2c8ba7a95d139369d0ecff36f7576ec403faad
Andrew Darts <mandrewdarts@gmail.com>
2020-11-20 23:18:16 -0600

    init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+export type SubscriptionCallback<T> = (state: T) => void;
+
+type State = object;
+
+export class ProxyStore<T extends State> {
+  public state: any;
+  private subscriptions = new Set<SubscriptionCallback<T>>();
+
+  constructor(private _init: T) {
+    this.state = new Proxy<T>(this._init, {
+      set: (target: any, key: string, value: any) => {
+        target[key] = value;
+        this.subscriptions.forEach((fn: SubscriptionCallback<T>) => fn(target));
+        return true;
+      },
+
+      get: (target: any, key: any) => {
+        return target[key];
+      },
+    });
+  }
+
+  subscribe(fn: SubscriptionCallback<T>) {
+    this.subscriptions.add(fn);
+    return () => this.subscriptions.delete(fn);
+  }
+
+  update(fn: (state: T) => T) {
+    const newState: any = fn(this.state);
+    Object.keys(newState).forEach(
+      (key: string | number) => (this.state[key] = newState[key])
+    );
+  }
+
+  get(): T {
+    return this.state;
+  }
+
+  select(fn: (state: T) => any) {
+    return fn(this.state);
+  }
+}
---
+++ test/blah.test.ts
@@ -0,0 +1,75 @@
+import { ProxyStore } from '../src';
+
+describe('ProxyStore', () => {
+  it('should intialize a store', () => {
+    const p = new ProxyStore({ name: 'Luke' });
+
+    expect(p.get().name).toEqual('Luke');
+  });
+
+  it('should be updatable', () => {
+    const p = new ProxyStore({ name: 'Luke' });
+    p.state.name = 'Han';
+
+    expect(p.get().name).toEqual('Han');
+  });
+
+  it('should handle selectors', () => {
+    const p = new ProxyStore({
+      user: { name: { first: 'Ben', last: 'Solo' } },
+    });
+    const firstName = p.select(state => state.user.name.first);
+    expect(firstName).toEqual('Ben');
+  });
+
+  it('should handle subscriptions', () => {
+    const p = new ProxyStore({ name: 'Luke' });
+    const sub = jest.fn();
+    p.subscribe(sub);
+
+    p.update(state => ({ ...state, name: 'Han' }));
+
+    expect(sub).toHaveBeenCalled();
+  });
+
+  it('should handle nested subscriptions', () => {
+    const p = new ProxyStore({
+      user: { first_name: 'Luke', last_name: 'Skywalker' },
+    });
+    const sub = jest.fn();
+    p.subscribe(sub);
+
+    p.update(state => {
+      state.user.first_name = 'Rey';
+      return state;
+    });
+
+    expect(sub).toHaveBeenCalled();
+    expect(p.state.user.first_name).toEqual('Rey');
+  });
+
+  it('should remove subscriptions', () => {
+    const p = new ProxyStore({
+      user: { first_name: 'Luke', last_name: 'Skywalker' },
+    });
+
+    const sub = jest.fn();
+    const unsub = p.subscribe(sub);
+
+    p.update(state => {
+      state.user.first_name = 'Rey';
+      return state;
+    });
+
+    expect(sub).toHaveBeenCalledTimes(1);
+
+    unsub();
+
+    p.update(state => {
+      state.user.first_name = 'Luke';
+      return state;
+    });
+
+    expect(sub).toHaveBeenCalledTimes(1);
+  });
+});
